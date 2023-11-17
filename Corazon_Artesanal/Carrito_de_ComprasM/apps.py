4f6f234df268c75c27ef5b90689bb631f5fa8fa6
Guillermo Veyro <memoveyrojr@hotmail.com>
2023-11-17 15:27:10 -0600

    1

---
+++ Corazon_Artesanal/Carrito_de_ComprasM/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarritoDeComprasmConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Carrito_de_ComprasM"
---
+++ Corazon_Artesanal/Gestion_de_Usuarios_tipo_CompradorM/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionDeUsuariosTipoCompradormConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Gestion_de_Usuarios_tipo_CompradorM"
