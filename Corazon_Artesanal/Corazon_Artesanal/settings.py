3be5dc342d59043720a6bf8b92b63ea3cf969f36
OzielLM <oziel_bear@icloud.com>
2023-11-17 17:20:37 -0600

    Creacion de el modulo y correcciones

--- Corazon_Artesanal/Corazon_Artesanal/settings.py
+++ Corazon_Artesanal/Corazon_Artesanal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "Gestion_de_Usuarios_tipo_CompradorM.apps.GestionDeUsuariosTipoCompradormConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
---
+++ Corazon_Artesanal/Historial_Compra_Venta/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HistorialCompraVentaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Historial_Compra_Venta'
