095281bc0a155bd3c59885fab8a6c69a64ba3f16
Rogelio <rogelioceballos218@gmail.com>
2023-11-17 16:48:45 -0600

    Auto stash before merge of "Carrito_Compras" and "origin/Carrito_Compras"
    commit de carrito e historial de compras y ventas

--- Corazon_Artesanal/Carrito_de_ComprasM/models.py
+++ Corazon_Artesanal/Carrito_de_ComprasM/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from Gestion_de_Usuarios_tipo_CompradorM.models import Comprador
 
-class Obra(models.Model):
-    id_obra = models.IntegerField(unique=True)
-    id_artesano = models.IntegerField(unique=True)
-    nombre = models.CharField(max_length="50")
-    precio_decimal = models.DecimalField(max_digits="7", decimal_places="2")
-    descripcion = models.CharField(max_length="300")
-    proceso_creatio = models.CharField(max_length="50")
+class CarritoCompra(models.Model):
+    CarritoID = models.AutoField(primary_key=True)
+    comprador = models.ForeignKey(Comprador)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+class DetalleCarrito(models.Model):
+    DetalleCarritoID = models.AutoField(primary_key=True)
+    CarritoID = models.ForeignKey(CarritoCompra)
+    obra = models.ForeignKey(Obra)
+    cantidad = models.PositiveIntegerField()
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+
     
-    
-    def __str__(self):
-        return self.nombre
