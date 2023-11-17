fd1cf4144f1590ea30cc5b7befb9ca8f8acde1d6
Guillermo Veyro <memoveyrojr@hotmail.com>
2023-11-17 16:05:29 -0600

    Commit modelos Inicio sesion Artesano

--- Corazon_Artesanal/Gestion_de_Usuarios_tipo_ArtesanosM/models.py
+++ Corazon_Artesanal/Gestion_de_Usuarios_tipo_ArtesanosM/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class InformacionPublica(models.Model):
+    id_informacion_publica = models.IntegerField()
+    nombre = models.CharField(max_length=60)
+    telefono = models.CharField(max_length= 10)
+    historia = models.TextField(max_length=1000)
+    correo_electronico = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.correo_electronico, self.historia
+        
+
+
+class InformacionPrivada(models.Model):
+    id_informacion_publica = models.ForeignKey()
+    fecha_nacimiento = models.DateField()
+    contrasena = models.CharField(max_length=8)
+
